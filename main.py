a3586b6e9363b5b31c02164f093b516a1f6e0188
Julen <julenbujanda@me.com>
2020-11-11 21:46:37 +0000

    Return results

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+import hashlib
 
 
 class DemoExtension(Extension):
@@ -18,11 +19,19 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
         query = event.get_argument() or str()
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description=query,
-                                             on_enter=HideWindowAction()))
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name='MD5',
+                                         description=hashlib.md5(query.encode('utf-8')).hexdigest(),
+                                         on_enter=HideWindowAction()))
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name='SHA256',
+                                         description=hashlib.sha256(query.encode('utf-8')).hexdigest(),
+                                         on_enter=HideWindowAction()))
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name='SHA3-256',
+                                         description=hashlib.sha3_256(query.encode('utf-8')).hexdigest(),
+                                         on_enter=HideWindowAction()))
+
 
         return RenderResultListAction(items)
 
