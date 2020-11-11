0a49f686c05d07bea42be9100a1ade287431c64b
Julen <julenbujanda@me.com>
2020-11-11 21:27:00 +0000

    Initial main commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+
+class DemoExtension(Extension):
+
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+        query = event.get_argument() or str()
+        for i in range(5):
+            items.append(ExtensionResultItem(icon='images/icon.png',
+                                             name='Item %s' % i,
+                                             description=query,
+                                             on_enter=HideWindowAction()))
+
+        return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    DemoExtension().run()
