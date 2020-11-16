5a7501b368dcd419a5ec42278f5e2cfd49fa3b76
Julen <julenbujanda@me.com>
2020-11-16 10:08:38 +0100

    Copy to clipboard

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 import hashlib
 
 
-class DemoExtension(Extension):
+class HashExtension(Extension):
 
     def __init__(self):
-        super(DemoExtension, self).__init__()
+        super(HashExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
 
@@ -19,26 +20,29 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
         query = event.get_argument() or str()
+        md5 = hashlib.md5(query.encode('utf-8')).hexdigest()
+        sha256 = hashlib.sha256(query.encode('utf-8')).hexdigest()
+        sha3_256 = description = hashlib.sha3_256(query.encode('utf-8')).hexdigest()
+        sha3_512 = hashlib.sha3_512(query.encode('utf-8')).hexdigest()
         items.append(ExtensionResultItem(icon='images/icon.png',
                                          name='MD5',
-                                         description=hashlib.md5(query.encode('utf-8')).hexdigest(),
-                                         on_enter=HideWindowAction()))
+                                         description=md5,
+                                         on_enter=CopyToClipboardAction(md5)))
         items.append(ExtensionResultItem(icon='images/icon.png',
                                          name='SHA256',
-                                         description=hashlib.sha256(query.encode('utf-8')).hexdigest(),
-                                         on_enter=HideWindowAction()))
+                                         description=sha256,
+                                         on_enter=CopyToClipboardAction(sha256)))
         items.append(ExtensionResultItem(icon='images/icon.png',
                                          name='SHA3-256',
-                                         description=hashlib.sha3_256(query.encode('utf-8')).hexdigest(),
-                                         on_enter=HideWindowAction()))
+                                         description=sha3_256,
+                                         on_enter=CopyToClipboardAction(sha3_256)))
         items.append(ExtensionResultItem(icon='images/icon.png',
                                          name='SHA3-512',
-                                         description=hashlib.sha3_512(query.encode('utf-8')).hexdigest(),
-                                         on_enter=HideWindowAction()))
+                                         description=sha3_512,
+                                         on_enter=CopyToClipboardAction(sha3_512)))
 
         return RenderResultListAction(items)
 
-#TODO: copy hash on enter key
 
 if __name__ == '__main__':
-    DemoExtension().run()
+    HashExtension().run()
